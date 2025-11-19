13e5fa1a7d3f1ef018e6cbbb9bcbab0f1f7fdeee
QuaternionWake <simep@proton.me>
2025-11-19 14:13:37 +0100

    Rename module to "num-parse"

--- build.zig
+++ build.zig
@@ -4,18 +4,18 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const mod = b.addModule("qw_parse", .{
+    const num_parse = b.addModule("num-parse", .{
         .root_source_file = b.path("src/num-parse.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const mod_tests = b.addTest(.{
-        .root_module = mod,
+    const num_parse_tests = b.addTest(.{
+        .root_module = num_parse,
     });
 
-    const run_mod_tests = b.addRunArtifact(mod_tests);
+    const run_tests = b.addRunArtifact(num_parse_tests);
 
     const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_mod_tests.step);
+    test_step.dependOn(&run_tests.step);
 }
