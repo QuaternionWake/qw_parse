c8ca1a53952fa645bb5a754d4190be77982cc94a
QuaternionWake <simep@proton.me>
2026-04-05 03:22:32 +0200

    Initial implementation on parseFloat()

--- src/num-parse.zig
+++ src/num-parse.zig
@@ -4,7 +4,7 @@ const ascii = std.ascii;
 const fmt = std.fmt;
 const math = std.math;
 
-const ParseError = error{
+const ParseIntError = error{
     InvalidCharacter,
     UnknownSuffix,
     NoNumber,
@@ -24,7 +24,7 @@ const ParseError = error{
 /// All the suffixes are case insensetive, and short and long ones allow
 /// whitespace between them and the number.
 /// E-suffixes don't allow whitespace and must be positive.
-pub fn parseInt(T: type, string: []const u8) ParseError!T {
+pub fn parseInt(T: type, string: []const u8) ParseIntError!T {
     const type_info = switch (@typeInfo(T)) {
         .int => |info| info,
         else => @compileError("found type " ++ @typeName(T) ++ ", parseInt only supports integers"),
@@ -33,7 +33,7 @@ pub fn parseInt(T: type, string: []const u8) ParseError!T {
 
     // Parse the string into a more managable format
     // Most of the more complex functionality is handled in this function
-    const chunks: NumberChunks = try .init(string);
+    const chunks: IntChunks = try .init(string);
 
     // At least one of these two needs to exist
     if (chunks.integer == null and chunks.decimal == null) return error.NoNumber;
@@ -88,7 +88,7 @@ pub fn parseInt(T: type, string: []const u8) ParseError!T {
         -signed_result;
 }
 
-fn convertError(err: fmt.ParseIntError, sign: NumberChunks.Sign) ParseError {
+fn convertError(err: fmt.ParseIntError, sign: IntChunks.Sign) ParseIntError {
     return switch (err) {
         error.Overflow => switch (sign) {
             .pos => error.Overflow,
@@ -98,7 +98,7 @@ fn convertError(err: fmt.ParseIntError, sign: NumberChunks.Sign) ParseError {
     };
 }
 
-const NumberChunks = struct {
+const IntChunks = struct {
     integer: ?[]const u8,
     decimal: ?[]const u8,
 
@@ -107,7 +107,7 @@ const NumberChunks = struct {
 
     const Sign = enum { pos, neg };
 
-    pub fn init(string_: []const u8) ParseError!NumberChunks {
+    pub fn init(string_: []const u8) ParseIntError!IntChunks {
         const string = mem.trim(u8, string_, &std.ascii.whitespace);
 
         if (string.len == 0) return error.NoNumber;
@@ -195,7 +195,7 @@ const digits = "0123456789";
 
 const t = std.testing;
 
-test "Basic parsing" {
+test "Int: Basic parsing" {
     try t.expectEqual(123, parseInt(i32, "123"));
     try t.expectEqual(123, parseInt(i32, "+123"));
     try t.expectEqual(-123, parseInt(i32, "-123"));
@@ -204,26 +204,26 @@ test "Basic parsing" {
     try t.expectEqual(123, parseInt(u32, "+123"));
 }
 
-test "Multiple signs" {
+test "Int: Multiple signs" {
     try t.expectError(error.InvalidCharacter, parseInt(i32, "++123"));
     try t.expectError(error.InvalidCharacter, parseInt(i32, "--123"));
     try t.expectError(error.InvalidCharacter, parseInt(i32, "+-123"));
     try t.expectError(error.InvalidCharacter, parseInt(i32, "-+123"));
 }
 
-test "Overflows" {
+test "Int: Overflows" {
     try t.expectError(error.Overflow, parseInt(i32, "999999999999"));
     try t.expectError(error.Underflow, parseInt(i32, "-999999999999"));
     try t.expectError(error.Underflow, parseInt(u32, "-123"));
 }
 
-test "Leading zeros" {
+test "Int: Leading zeros" {
     try t.expectEqual(123, parseInt(i32, "0000123"));
     try t.expectEqual(123, parseInt(i32, "+0000123"));
     try t.expectEqual(-123, parseInt(i32, "-0000123"));
 }
 
-test "Parsing zero" {
+test "Int: Parsing zero" {
     try t.expectEqual(0, parseInt(i32, "0"));
     try t.expectEqual(0, parseInt(i32, "-0"));
     try t.expectEqual(0, parseInt(i32, "+0"));
@@ -231,7 +231,7 @@ test "Parsing zero" {
     try t.expectEqual(0, parseInt(u32, "-0"));
 }
 
-test "Parsing limits" {
+test "Int: Parsing limits" {
     try t.expectEqual(-128, parseInt(i8, "-128"));
     try t.expectEqual(127, parseInt(i8, "127"));
     try t.expectEqual(0, parseInt(u8, "0"));
@@ -263,20 +263,20 @@ test "Parsing limits" {
     try t.expectError(error.Underflow, parseInt(i11, "-1025"));
 }
 
-test "Invalid characters" {
+test "Int: Invalid characters" {
     try t.expectError(error.InvalidCharacter, parseInt(i32, "12asdf456"));
     try t.expectError(error.NoNumber, parseInt(i32, "ten"));
     try t.expectError(error.NoNumber, parseInt(i32, ""));
     try t.expectError(error.InvalidCharacter, parseInt(i32, "123/456"));
 }
 
-test "Underscores not supported" {
+test "Int: Underscores not supported" {
     try t.expectError(error.InvalidCharacter, parseInt(i32, "1_2_3"));
     try t.expectError(error.InvalidCharacter, parseInt(i32, "123_"));
     try t.expectError(error.InvalidCharacter, parseInt(i32, "_123"));
 }
 
-test "Ten is greater than max(T)" {
+test "Int: Ten is greater than max(T)" {
     try t.expectEqual(3, parseInt(u2, "3"));
     try t.expectEqual(3, parseInt(i3, "3"));
     try t.expectEqual(-4, parseInt(i3, "-4"));
@@ -284,7 +284,7 @@ test "Ten is greater than max(T)" {
     try t.expectError(error.Underflow, parseInt(u2, "-1"));
 }
 
-test "Basic suffix functionality" {
+test "Int: Basic suffix functionality" {
     try t.expectEqual(123_000, parseInt(i32, "123k"));
     try t.expectEqual(123_000, parseInt(i32, "+123k"));
     try t.expectEqual(-123_000, parseInt(i32, "-123k"));
@@ -292,7 +292,7 @@ test "Basic suffix functionality" {
     try t.expectEqual(123_000_000_000, parseInt(i64, "123B"));
 }
 
-test "Spaces and tabs before suffix are allowed" {
+test "Int: Spaces and tabs before suffix are allowed" {
     try t.expectEqual(123_000, parseInt(i32, "123 k"));
     try t.expectEqual(123_000, parseInt(i32, "123    k"));
     try t.expectEqual(123_000, parseInt(i32, "123\tk"));
@@ -303,21 +303,21 @@ test "Spaces and tabs before suffix are allowed" {
     try t.expectError(error.InvalidCharacter, parseInt(i32, "123  \n k"));
 }
 
-test "Suffixes are case insensetive" {
+test "Int: Suffixes are case insensetive" {
     try t.expectEqual(123_000, parseInt(i32, "123 K"));
     try t.expectEqual(123_000_000, parseInt(i32, "123 m"));
     try t.expectEqual(123_000_000_000_000_000, parseInt(i64, "123 qa"));
     try t.expectEqual(123_000_000_000_000_000, parseInt(i64, "123 qA"));
 }
 
-test "Long suffixes" {
+test "Int: Long suffixes" {
     try t.expectEqual(123_000, parseInt(i32, "123 thousand"));
     try t.expectEqual(123_000, parseInt(i32, "123thousand"));
     try t.expectEqual(123_000_000, parseInt(i32, "123 MILLION"));
     try t.expectEqual(123_000_000_000, parseInt(i64, "123 BiLLIoN"));
 }
 
-test "Invalid suffixes" {
+test "Int: Invalid suffixes" {
     try t.expectError(error.UnknownSuffix, parseInt(i32, "123 H"));
     try t.expectError(error.UnknownSuffix, parseInt(i32, "123 asdf"));
     try t.expectError(error.UnknownSuffix, parseInt(i32, "123 thousaand"));
@@ -328,12 +328,12 @@ test "Invalid suffixes" {
     try t.expectError(error.NoNumber, parseInt(i32, "Million"));
 }
 
-test "Base and exponent less then max(T), but their multiple is over" {
+test "Int: Base and exponent less then max(T), but their multiple is over" {
     try t.expectError(error.Overflow, parseInt(i16, "100k"));
     try t.expectError(error.Underflow, parseInt(i16, "-100k"));
 }
 
-test "Basic decimal dot functionality" {
+test "Int: Basic decimal dot functionality" {
     try t.expectEqual(123, parseInt(i32, "123.0"));
     try t.expectEqual(123, parseInt(i32, "123.0000"));
 
@@ -342,7 +342,7 @@ test "Basic decimal dot functionality" {
     try t.expectEqual(123_456, parseInt(i32, "123.456789 k"));
 }
 
-test "Rounding towards zero" {
+test "Int: Rounding towards zero" {
     try t.expectEqual(123, parseInt(i32, "123.4"));
     try t.expectEqual(123, parseInt(i32, "123.5"));
     try t.expectEqual(123, parseInt(i32, "123.6"));
@@ -355,7 +355,7 @@ test "Rounding towards zero" {
     try t.expectEqual(-123, parseInt(i32, "-0.1234 k"));
 }
 
-test "Trailing and leading dot are allowed" {
+test "Int: Trailing and leading dot are allowed" {
     try t.expectEqual(123, parseInt(i32, "123."));
     try t.expectEqual(123_000, parseInt(i32, "123.k"));
     try t.expectEqual(123_000, parseInt(i32, "123. k"));
@@ -365,22 +365,22 @@ test "Trailing and leading dot are allowed" {
     try t.expectError(error.NoNumber, parseInt(i32, "."));
 }
 
-test "Prefixes not allowed after dot" {
+test "Int: Prefixes not allowed after dot" {
     try t.expectError(error.InvalidCharacter, parseInt(i32, "123.+456"));
     try t.expectError(error.InvalidCharacter, parseInt(i32, "123.-456"));
 }
 
-test "Trailing zeros don't cause overflow" {
+test "Int: Trailing zeros don't cause overflow" {
     try t.expectEqual(123, parseInt(i8, ".123000 k"));
     try t.expectEqual(123, parseInt(i8, ".000123000 M"));
 }
 
-test "Integer and decimal part less then max(T), but their sum is over" {
+test "Int: Integer and decimal part less then max(T), but their sum is over" {
     try t.expectError(error.Overflow, parseInt(i16, "32.999k"));
     try t.expectError(error.Underflow, parseInt(i16, "-32.999k"));
 }
 
-test "E-suffixes" {
+test "Int: E-suffixes" {
     try t.expectEqual(123_000, parseInt(i32, "123e3"));
     try t.expectEqual(-123_000, parseInt(i32, "-123e3"));
     try t.expectEqual(123_000_000, parseInt(i32, "123e6"));
@@ -389,7 +389,7 @@ test "E-suffixes" {
     try t.expectEqual(0, parseInt(i32, "0e200"));
 }
 
-test "Invalid e-suffixes" {
+test "Int: Invalid e-suffixes" {
     try t.expectError(error.InvalidCharacter, parseInt(i32, "123 e3"));
     try t.expectError(error.InvalidCharacter, parseInt(i32, "123e 3"));
 
@@ -399,9 +399,197 @@ test "Invalid e-suffixes" {
     try t.expectError(error.NoNumber, parseInt(i32, "e123"));
 }
 
-test "E-suffix overflow" {
+test "Int: E-suffix overflow" {
     try t.expectError(error.Overflow, parseInt(i32, "1e100000"));
     try t.expectError(error.Underflow, parseInt(i32, "-1e100000"));
 
     try t.expectEqual(0, parseInt(i32, "0e10000000000"));
+}
+
+const ParseFloatError = error{
+    InvalidCharacter,
+    UnknownSuffix,
+    NoNumber,
+};
+
+/// Parses `string` as a base 10 float of type `T`.
+/// Numbers can be written with a decimal dot and a suffix denoting their order
+/// of magnitude.
+///
+/// Three types of suffixes can be used:
+///  * Short suffixes (e.g. "1k", "2M", "3Qa")
+///  * Long suffixes (e.g. "1 thousand", "2 million", "3 quadrillion")
+///  * E-suffixes (scientific/engineering suffixes, e.g "1e3, 2e6, 3e15")
+///
+/// All the suffixes are case insensetive.
+/// Short and long suffixes allow whitespace between them and the number,
+/// e-suffixes don't.
+fn parseFloat(T: type, string: []const u8) ParseFloatError!T {
+    if (@typeInfo(T) != .float) {
+        @compileError("found type " ++ @typeName(T) ++ ", parseInt only supports integers");
+    }
+
+    const chunks: FloatChunks = try .init(string);
+
+    const pow_of_10 = math.pow(T, 10, @floatFromInt(chunks.order_of_magnitude));
+    const value = try fmt.parseFloat(T, chunks.number);
+
+    return value * pow_of_10;
+}
+
+const FloatChunks = struct {
+    number: []const u8,
+    order_of_magnitude: u8,
+
+    pub fn init(string_: []const u8) ParseFloatError!FloatChunks {
+        const string = mem.trim(u8, string_, &ascii.whitespace);
+
+        if (string.len == 0) return error.NoNumber;
+
+        // Deal with special cases at the start
+        const num_start: usize = if (string[0] == '+' or string[0] == '-') 1 else 0;
+        if (ascii.eqlIgnoreCase(string[num_start..], "inf") or
+            ascii.eqlIgnoreCase(string[num_start..], "infinity") or
+            ascii.eqlIgnoreCase(string[num_start..], "nan"))
+        {
+            return .{ .number = string, .order_of_magnitude = 0 };
+        }
+
+        const num_end = mem.indexOfNone(u8, string, digits ++ "Ee+-.") orelse string.len;
+
+        const oom, const has_suffix = blk: {
+            const suffix_start = if (mem.lastIndexOfNone(u8, string, ascii.letters)) |idx|
+                idx + 1
+            else
+                return error.NoNumber;
+            // Make there is only allowed whitespace between two the end of the number and start of the suffix
+            if (mem.indexOfNone(u8, string[num_end..suffix_start], allowed_whitespace) != null) return error.InvalidCharacter;
+            const suffix_str = mem.trim(u8, string[suffix_start..], allowed_whitespace);
+            if (suffix_str.len == 0) break :blk .{ 0, false };
+            break :blk .{ try parseSuffix(suffix_str), true };
+        };
+
+        const number = string[0..num_end];
+
+        // Make sure there is at least one actual digit
+        if (mem.indexOfAny(u8, number, digits) == null) return error.NoNumber;
+
+        // Make sure only one kind of suffix is present
+        if (has_suffix and mem.indexOfAny(u8, number, "Ee") != null) return error.InvalidCharacter;
+
+        // Multiple dots, Es, or minuses or pluses in invalid places get
+        // checked for by fmt.parseFloat anyway, so there's no need to check
+        // for those here
+
+        return .{
+            .number = number,
+            .order_of_magnitude = oom,
+        };
+    }
+};
+
+test "Float: Basic float parsing" {
+    try t.expectEqual(123, parseFloat(f32, "123"));
+    try t.expectEqual(123, parseFloat(f32, "+123"));
+    try t.expectEqual(-123, parseFloat(f32, "-123"));
+    try t.expectEqual(123, parseFloat(f32, "123.0"));
+    try t.expectEqual(123.456, parseFloat(f32, "123.456"));
+
+    try t.expectEqual(123, parseFloat(f32, "123."));
+    try t.expectEqual(0.123, parseFloat(f32, ".123"));
+    try t.expectEqual(-0.123, parseFloat(f32, "-.123"));
+    try t.expectEqual(123_000, parseFloat(f32, "123.e3"));
+
+    try t.expectError(error.InvalidCharacter, parseFloat(f32, "++123"));
+    try t.expectError(error.InvalidCharacter, parseFloat(f32, "--123"));
+    try t.expectError(error.InvalidCharacter, parseFloat(f32, "+-123"));
+    try t.expectError(error.InvalidCharacter, parseFloat(f32, "-+123"));
+
+    try t.expectEqual(123, parseFloat(f32, "0000123"));
+    try t.expectEqual(123, parseFloat(f32, "+0000123"));
+    try t.expectEqual(-123, parseFloat(f32, "-0000123"));
+
+    try t.expectEqual(0.0, parseFloat(f32, "0"));
+    try t.expectEqual(0.0, parseFloat(f32, "+0"));
+    try t.expectEqual(-0.0, parseFloat(f32, "-0"));
+}
+
+test "Float: Overflows, infinity, NaN" {
+    try t.expectEqual(math.inf(f32), parseFloat(f32, "999999999999999999999999999999999999999999"));
+    try t.expectEqual(-math.inf(f32), parseFloat(f32, "-999999999999999999999999999999999999999999"));
+
+    try t.expectEqual(math.inf(f32), parseFloat(f32, "inf"));
+    try t.expectEqual(math.inf(f32), parseFloat(f32, "+INF"));
+    try t.expectEqual(-math.inf(f32), parseFloat(f32, "-iNf"));
+
+    try t.expectEqual(math.inf(f32), parseFloat(f32, "infinity"));
+    try t.expectEqual(math.inf(f32), parseFloat(f32, "InFInItY"));
+
+    try t.expect(math.isNan(try parseFloat(f32, "nan")));
+    try t.expect(math.isNan(try parseFloat(f32, "-NaN")));
+}
+
+test "Float: Invalid characters" {
+    try t.expectError(error.InvalidCharacter, parseFloat(f32, "12asdf456"));
+    try t.expectError(error.InvalidCharacter, parseFloat(f32, "123/456"));
+    try t.expectError(error.InvalidCharacter, parseFloat(f32, "123..456"));
+    try t.expectError(error.InvalidCharacter, parseFloat(f32, "123.45.6"));
+    try t.expectError(error.InvalidCharacter, parseFloat(f32, "123e4.56"));
+
+    try t.expectError(error.NoNumber, parseFloat(f32, "ten"));
+    try t.expectError(error.NoNumber, parseFloat(f32, ""));
+
+    try t.expectError(error.InvalidCharacter, parseFloat(f32, "1_2_3"));
+    try t.expectError(error.InvalidCharacter, parseFloat(f32, "123_"));
+    try t.expectError(error.InvalidCharacter, parseFloat(f32, "_123"));
+}
+
+test "Float: E-suffixes" {
+    try t.expectEqual(123, parseFloat(f32, "1.23e2"));
+    try t.expectEqual(123, parseFloat(f32, "1.23E2"));
+    try t.expectEqual(123, parseFloat(f32, "12.3e1"));
+    try t.expectEqual(123, parseFloat(f32, "0.00123e5"));
+    try t.expectEqual(123, parseFloat(f32, "0.00123e+5"));
+    try t.expectEqual(123, parseFloat(f32, "123000e-3"));
+
+    try t.expectEqual(math.inf(f32), parseFloat(f32, "123e9999999999999999999999"));
+    try t.expectEqual(0, parseFloat(f32, "0e9999999999999999999999"));
+    try t.expectEqual(0, parseFloat(f32, "123e-9999999999999999999999"));
+}
+
+test "Float: Long and short suffixes" {
+    try t.expectEqual(123_000, parseFloat(f32, "123k"));
+    try t.expectEqual(123_000, parseFloat(f32, "+123k"));
+    try t.expectEqual(-123_000, parseFloat(f32, "-123k"));
+    try t.expectEqual(123_000_000, parseFloat(f32, "123M"));
+    try t.expectEqual(123_000_000_000, parseFloat(f64, "123B"));
+    try t.expectEqual(123_000_000_000_000_000, parseFloat(f64, "123qa"));
+    try t.expectEqual(123_000_000_000_000_000, parseFloat(f64, "123qA"));
+    try t.expectEqual(123456.789, parseFloat(f32, "123.456789k"));
+
+    try t.expectEqual(123_000, parseFloat(f32, "123 thousand"));
+    try t.expectEqual(123_000, parseFloat(f32, "123thousand"));
+    try t.expectEqual(123_000_000, parseFloat(f32, "123 MILLION"));
+    try t.expectEqual(123_000_000_000, parseFloat(f64, "123 BiLLIoN"));
+
+    try t.expectEqual(123_000, parseFloat(f32, "123 k"));
+    try t.expectEqual(123_000, parseFloat(f32, "123    K"));
+    try t.expectEqual(123_000, parseFloat(f32, "123\tk"));
+    try t.expectEqual(123_000, parseFloat(f32, "123  \tk"));
+
+    try t.expectError(error.InvalidCharacter, parseFloat(f32, "123\nk"));
+    try t.expectError(error.InvalidCharacter, parseFloat(f32, "123\rk"));
+    try t.expectError(error.InvalidCharacter, parseFloat(f32, "123  \n k"));
+
+    try t.expectError(error.UnknownSuffix, parseFloat(f32, "123 H"));
+    try t.expectError(error.UnknownSuffix, parseFloat(f32, "123 asdf"));
+    try t.expectError(error.UnknownSuffix, parseFloat(f32, "123 thousaand"));
+
+    try t.expectError(error.InvalidCharacter, parseFloat(f32, "123 abc def"));
+    try t.expectError(error.InvalidCharacter, parseFloat(f32, "123 thousand thousand"));
+
+    try t.expectError(error.NoNumber, parseFloat(f32, "Million"));
+
+    try t.expectError(error.InvalidCharacter, parseFloat(f32, "123e3 k"));
+    try t.expectError(error.InvalidCharacter, parseFloat(f32, "123e3 million"));
 }
